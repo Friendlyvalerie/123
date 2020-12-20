cb27756e17790b1477fd61329df0332952ed18d6
Friendlyvalerie <51228323+Friendlyvalerie@users.noreply.github.com>
2020-12-20 23:41:57 +0300

    Add project files.

---
+++ 123/meek.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _123
+{
+    public partial class meek : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 123/val.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/meek.Master" AutoEventWireup="true" CodeBehind="val.aspx.cs" Inherits="_123.val" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
